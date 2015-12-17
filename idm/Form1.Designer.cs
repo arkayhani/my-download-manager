63c339e92cae1e4b1afc21b28999467669fe1906
arkayhani <arkayhani@gmail.com>
2015-12-17 03:40:06 +0330

    Download manager

---
+++ idm/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace idm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(465, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(465, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(603, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Link address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(604, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "save address";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(652, 173);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(13, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(652, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 129);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(166, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(186, 129);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(164, 23);
+            this.progressBar2.TabIndex = 10;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(356, 129);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(152, 23);
+            this.progressBar3.TabIndex = 11;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(514, 129);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(151, 23);
+            this.progressBar4.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "idle";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(132, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Number of States:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Download State:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 360);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ idm/Form1.cs
@@ -0,0 +1,395 @@
+﻿////////////////////Arash Kayhani 9023013
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+
+namespace idm
+{
+    public partial class Form1 : Form
+    {
+
+        int segmentnum = 4;
+
+
+        List<file> queue = new List<file>();
+        long downdize = 0;
+        private readonly AutoResetEvent _signal = new AutoResetEvent(false);
+        private readonly AutoResetEvent _signal2 = new AutoResetEvent(false);
+        private readonly AutoResetEvent _signal3 = new AutoResetEvent(true);
+        private readonly AutoResetEvent _signal4 = new AutoResetEvent(true);
+        private delegate void UpdateProgessCallback(Int64 BytesRead, Int64 TotalBytes);
+        class file
+        {
+            public string url;
+            public string saveaddress;
+        }
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.DefaultConnectionLimit = 1000;
+            System.Net.ServicePointManager.MaxServicePointIdleTime = 10000;
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        void start()
+        {
+            foreach (file item in queue)
+            {
+                segmentnum = int.Parse(textBox3.Text);
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                progressBar3.Value = 0;
+                progressBar4.Value = 0;
+                listBox1.SelectedIndex = queue.IndexOf(item);
+                filedownload(item);
+
+            }
+
+        }
+        object[] selfhealing1(string url, long total, object len)
+        {
+
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create((string)url);
+            HttpWebResponse myHttpWebResponse;
+
+            object[] args = new object[2];
+
+            while (true)
+            {
+                try
+                {
+
+                    myHttpWebRequest.AddRange(total, (long)len);
+
+
+
+
+                    myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+                    args[0] = myHttpWebRequest;
+                    args[1] = myHttpWebResponse;
+                    break;
+                }
+                catch (Exception)
+                {
+
+
+                    Thread.Sleep(1000);
+                    label3.Text = "connecting...";
+
+                }
+            }
+
+            return args;
+
+
+
+        }
+        Stream selfhealing2(HttpWebRequest myHttpWebRequest, HttpWebResponse myHttpWebResponse, Stream streamResponse, long total, object len, object url)
+        {
+
+            _signal4.WaitOne();
+            label3.Text = "connecting...";
+            while (true)
+            {
+                try
+                {
+                    if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+                    {
+
+                        myHttpWebRequest = (HttpWebRequest)WebRequest.Create((string)url);
+
+                        myHttpWebRequest.AddRange(total, ((long)len));
+                        myHttpWebRequest.Timeout = 10000;
+
+                        myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+
+                        streamResponse = myHttpWebResponse.GetResponseStream();
+
+                        break;
+
+                    }
+
+
+
+
+
+                }
+                catch (Exception)
+                {
+
+                    streamResponse.Flush();
+                    myHttpWebRequest.KeepAlive = false;
+
+                    streamResponse.Close();
+                    streamResponse.Dispose();
+                    myHttpWebResponse.Close();
+                    myHttpWebResponse.Dispose();
+                    myHttpWebRequest.Abort();
+                    label3.Text = "connecting...";
+                }
+            }
+            _signal4.Set();
+            return streamResponse;
+
+
+        }
+        private void partdownload(object args)
+        {
+            Array argArray = new object[5];
+            argArray = (Array)args;
+            string url = (string)argArray.GetValue(0);
+            string path = (string)argArray.GetValue(1);
+            object len = argArray.GetValue(2);
+            object start = argArray.GetValue(3);
+            object index = argArray.GetValue(4);
+
+            Stream strLocal = new FileStream((string)path, FileMode.Create, FileAccess.Write, FileShare.None);
+
+            long total = (long)start;
+
+
+
+
+
+            object[] https = selfhealing1(url, total, len);
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)https[0];
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)https[1];
+
+            Int64 fileSize = myHttpWebResponse.ContentLength;
+
+
+            Stream streamResponse = myHttpWebResponse.GetResponseStream();
+            _signal2.Set();
+            int bytesSize = 0;
+
+
+            byte[] downBuffer = new byte[2048];
+            int counter = 1000;
+
+            while (total < (long)len)
+            {
+
+                if (counter == 0)
+                {
+
+                    counter = 1000;
+
+
+                    streamResponse = selfhealing2(myHttpWebRequest, myHttpWebResponse, streamResponse, total, len, url);
+                }
+                try
+                {
+
+                    downBuffer = new byte[2048];
+                    Application.DoEvents();
+                    streamResponse.ReadTimeout = 2000;
+                    bytesSize = streamResponse.Read(downBuffer, 0, downBuffer.Length - 10);
+                }
+                catch (Exception)
+                {
+
+                    streamResponse = selfhealing2(myHttpWebRequest, myHttpWebResponse, streamResponse, total, len, url);
+                    continue;
+                }
+
+                total += bytesSize;
+                downdize += bytesSize;
+
+                strLocal.Write(downBuffer, 0, bytesSize);
+                if (((int)index) == 0)
+                    progressBar1.Invoke(new UpdateProgessCallback(this.UpdateProgress), new object[] { total - (long)start, (long)len - (long)start });
+                else if (((int)index) == 1)
+                    progressBar2.Invoke(new UpdateProgessCallback(this.UpdateProgress1), new object[] { total - (long)start, (long)len - (long)start });
+                else if (((int)index) == 2)
+                    progressBar3.Invoke(new UpdateProgessCallback(this.UpdateProgress2), new object[] { total - (long)start, (long)len - (long)start });
+                else if (((int)index) == 3)
+                    progressBar4.Invoke(new UpdateProgessCallback(this.UpdateProgress3), new object[] { total - (long)start, (long)len - (long)start });
+                if (bytesSize == 0)
+                {
+                    counter--;
+                }
+            }
+
+            strLocal.Close();
+            streamResponse.Close();
+            myHttpWebResponse.Close();
+
+
+            _signal.Set();
+        }
+        private void filedownload(file f)
+        {
+            HttpWebRequest myHttpWebRequest;
+            HttpWebResponse myHttpWebResponse;
+            try
+            {
+                myHttpWebRequest = (HttpWebRequest)WebRequest.Create(f.url);
+
+
+                myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            }
+            catch (Exception)
+            {
+
+                object[] https = selfhealing1(f.url, 0, 10);
+                myHttpWebRequest = (HttpWebRequest)https[0];
+                myHttpWebResponse = (HttpWebResponse)https[1];
+
+            }
+
+            long fileSize = myHttpWebResponse.ContentLength;
+            myHttpWebResponse.Close();
+
+            long size = (long)(fileSize / segmentnum);
+            for (int i = 0; i < segmentnum; i++)
+            {
+
+                object ars;
+
+                if (i < segmentnum - 1)
+                    ars = new object[5] { f.url, f.saveaddress + i.ToString() + ".part", size * (i + 1), size * i, i };
+
+                else
+                    ars = new object[5] { f.url, (f.saveaddress + i.ToString() + ".part"), fileSize, size * i, i };
+
+                Thread thrDownload = new Thread(new ParameterizedThreadStart(partdownload));
+
+
+                thrDownload.Start(ars);
+
+                _signal2.WaitOne();
+
+
+            }
+
+            for (int i = 0; i < segmentnum; i++)
+            {
+                _signal.WaitOne();
+            }
+
+            label3.Text = "joining";
+            string s2 = f.saveaddress.Substring(0, f.saveaddress.LastIndexOf('\\'));
+            JoinFiles(s2, f.saveaddress);
+
+            DelFiles(s2);
+
+            downdize = 0;
+            label3.Text = "idle";
+
+        }
+
+        private void JoinFiles(string FolderInputPath, string FileOutputPath)
+        {
+
+            DirectoryInfo diSource = new DirectoryInfo(FolderInputPath);
+
+            FileStream fsSource = new FileStream(FileOutputPath, FileMode.Append);
+
+
+            foreach (FileInfo fiPart in diSource.GetFiles(@"*.part"))
+            {
+
+                Byte[] bytePart = System.IO.File.ReadAllBytes(fiPart.FullName);
+
+                fsSource.Write(bytePart, 0, bytePart.Length);
+            }
+
+            fsSource.Close();
+        }
+        private void DelFiles(string FolderInputPath)
+        {
+
+            DirectoryInfo diSource = new DirectoryInfo(FolderInputPath);
+
+            foreach (FileInfo fiPart in diSource.GetFiles(@"*.part"))
+            {
+
+                fiPart.Delete();
+            }
+
+        }
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            file f = new file();
+            f.url = textBox1.Text;
+            f.saveaddress = textBox2.Text;
+            listBox1.Items.Add(f.url);
+            queue.Add(f);
+            button1.Enabled = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+        private void browse_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.ShowDialog();
+            textBox2.Text = s.FileName;
+            button1.Enabled = true;
+
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Thread thr = new Thread(new ThreadStart(start));
+            thr.Start();
+
+        }
+        private void UpdateProgress(Int64 BytesRead, Int64 TotalBytes)
+        {
+
+            int PercentProgress = Convert.ToInt32((BytesRead * 100) / TotalBytes);
+
+            progressBar1.Value = PercentProgress;
+
+            label3.Text = "downloading";
+
+        }
+        private void UpdateProgress1(Int64 BytesRead, Int64 TotalBytes)
+        {
+
+            int PercentProgress = Convert.ToInt32((BytesRead * 100) / TotalBytes);
+
+            progressBar2.Value = PercentProgress;
+
+            label3.Text = "downloading";
+
+        }
+        private void UpdateProgress2(Int64 BytesRead, Int64 TotalBytes)
+        {
+
+            int PercentProgress = Convert.ToInt32((BytesRead * 100) / TotalBytes);
+
+            progressBar3.Value = PercentProgress;
+
+            label3.Text = "downloading";
+
+        }
+        private void UpdateProgress3(Int64 BytesRead, Int64 TotalBytes)
+        {
+            int PercentProgress = Convert.ToInt32((BytesRead * 100) / TotalBytes);
+
+            progressBar4.Value = PercentProgress;
+
+            label3.Text = "downloading";
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
